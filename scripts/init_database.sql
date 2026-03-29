2a91c5735c2da17c863c23faeaf2a6bc4f7e08ef
AsrinZandi <105767389+AsrinZandi@users.noreply.github.com>
2026-03-29 15:33:16 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,20 @@
 
+----Script purpose:
+----This scripts create new data base plus three difrent scema of bronze,silver and gold 
+
+Use master;
+-----create new database
+create database DataWarehouse;
+use DataWarehouse;
+---create new schema for bronze, silver and gold layers
+GO
+CREATE SCHEMA bronze;
+GO
+GO
+CREATE SCHEMA silver;
+GO
+GO
+CREATE SCHEMA gold;
+GO
+
+
